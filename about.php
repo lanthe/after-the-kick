cb3c9d08c4205536c361bd26ec5868c3de06cb52
lanthe <iolanthe.chronis@gmail.com>
2012-08-31 22:16:15 -0700

    [ikc] type selectors are now links

--- about.php
+++ about.php
@@ -8,7 +8,12 @@ include_once("footer.php");
 echo get_header();
 
 echo "<div class='content_area'>
-<p>We just wanted to be able to find and buy products after they'd been funded.  So we built a way to do that.</p>
+<p>We compile projects built with crowdfunding that can now be bought by the public.  If you're aware of a product that we've missed, please let us know by sending an <a href='mailto:kickfollower@gmail.com'>email</a>.</p>
+
+<p>Also let us know if you have any comments, words of praise or shame, or something else you'd like to share.</p>
+
+<p>Happy shopping!</p>
+
 </div>";
 
 echo get_footer();
