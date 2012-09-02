e55559e62182186bd3c18b4704f429e6fc414730
lanthe <iolanthe.chronis@gmail.com>
2012-09-02 14:05:29 -0700

    [ikc] preparing to automate database fill for amazon products

--- product_view.php
+++ product_view.php
@@ -5,7 +5,7 @@ function product_view($p) {
   return "<div class='content_area product_area'>
 			<div class='product_title'>".$p->name."</div>
 			<div class='product_img' style='background-image:url(\"".$p->img."\");'></div>
-			<div class='product_description'>".$p->description.".</div>
+			<div class='product_description'>".$p->description."</div>
 			<br>
 			<div class='product_price'>".$p->price."</div>
 			<a class='buy_button' href='".$p->url."'>BUY</a>
