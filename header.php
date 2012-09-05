8d61717af22aa0bde92d6c318447e33ab4f28a7b
lanthe <iolanthe.chronis@gmail.com>
2012-09-05 13:56:30 -0700

    [ikc] least sexy tagline ever

--- header.php
+++ header.php
@@ -47,6 +47,6 @@ Transitional//EN\">
     <div class='header'>
 		  <div class='header_link'><a class='header_link' href='about.php'>What?</a></div>
     </div><a class='logo_text'href='/'>".site_name()."</a>".
-    "<div class=tagline>Shop for crowdfunded creations</div>";
+    "<div class=tagline>Kickstarter projects that are finished and available in stores</div>";
 }
 ?>
